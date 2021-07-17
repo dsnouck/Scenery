c8ffcac2c8f03f4048b49a6ea5c39e7e5be5910d
Daniel Snouck <dsnouck@gmail.com>
2021-07-17 22:08:00 +0200

    Add CustomJsonConverter.

---
+++ SceneryWebApi/Scenery.Controllers/Converters/CustomJsonConverter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Scenery.Controllers.Converters
+{
+    public abstract class CustomJsonConverter<T> : JsonConverter<T>
+    {
+        protected static void ReadStartObject(ref Utf8JsonReader reader)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject && (!reader.Read() || reader.TokenType != JsonTokenType.StartObject))
+            {
+                throw new JsonException();
+            }
+        }
+
+        protected static void ReadPropertyName(ref Utf8JsonReader reader, string propertyName)
+        {
+            if (!reader.Read() || reader.TokenType != JsonTokenType.PropertyName)
+            {
+                throw new JsonException();
+            }
+
+            if (!string.Equals(reader.GetString(), propertyName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new JsonException();
+            }
+        }
+
+        protected static double ReadDoubleProperty(ref Utf8JsonReader reader, string propertyName)
+        {
+            ReadPropertyName(ref reader, propertyName);
+
+            if (!reader.Read() || reader.TokenType != JsonTokenType.Number)
+            {
+                throw new JsonException();
+            }
+
+            return reader.GetDouble();
+        }
+
+        protected static string ReadStringProperty(ref Utf8JsonReader reader, string propertyName)
+        {
+            ReadPropertyName(ref reader, propertyName);
+
+            if (!reader.Read() || reader.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException();
+            }
+
+            return reader.GetString();
+        }
+
+        protected static void ReadEndObject(ref Utf8JsonReader reader)
+        {
+            if (!reader.Read() || reader.TokenType != JsonTokenType.EndObject)
+            {
+                throw new JsonException();
+            }
+        }
+
+        protected static void WriteStartObject(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+        }
+
+        protected static void WritePropertyName(Utf8JsonWriter writer, string propertyName)
+        {
+            writer.WritePropertyName(ConvertToCamelCase(propertyName));
+        }
+
+        protected static void WriteNumber(Utf8JsonWriter writer, string propertyName, double value)
+        {
+            writer.WriteNumber(ConvertToCamelCase(propertyName), value);
+        }
+
+        protected static void WriteStringInCamelCase(Utf8JsonWriter writer, string propertyName, string value)
+        {
+            writer.WriteString(ConvertToCamelCase(propertyName), ConvertToCamelCase(value));
+        }
+
+        protected static void WriteEndObject(Utf8JsonWriter writer)
+        {
+            writer.WriteEndObject();
+
+        }
+
+        protected static string ConvertToCamelCase(string value)
+        {
+            return $"{char.ToLowerInvariant(value[0])}{value[1..]}";
+        }
+
+        protected static string ConvertToPascalCase(string value)
+        {
+            return $"{char.ToUpperInvariant(value[0])}{value[1..]}";
+        }
+    }
+}
--- SceneryWebApi/Scenery.Controllers/Converters/SceneJsonConverter.cs
+++ SceneryWebApi/Scenery.Controllers/Converters/SceneJsonConverter.cs
@@ -1,11 +1,10 @@
 ﻿using Scenery.Models.Scenes;
 using System;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Scenery.Controllers.Converters
 {
-    public class SceneJsonConverter : JsonConverter<Scene>
+    public class SceneJsonConverter : CustomJsonConverter<Scene>
     {
         public override Scene Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
@@ -14,6 +13,7 @@ namespace Scenery.Controllers.Converters
 
         private static Scene ReadScene(ref Utf8JsonReader reader)
         {
+            var scene = new Scene();
             ReadStartObject(ref reader);
             var type = ConvertToPascalCase(ReadStringProperty(ref reader, nameof(Type)));
             switch (type)
@@ -22,74 +22,18 @@ namespace Scenery.Controllers.Converters
                     var factor = ReadDoubleProperty(ref reader, nameof(ScaledScene.Factor));
                     ReadPropertyName(ref reader, nameof(ScaledScene.OriginalScene));
                     var originalScene = ReadScene(ref reader);
-                    ReadEndObject(ref reader);
-
-                    return new ScaledScene
+                    scene = new ScaledScene
                     {
                         Factor = factor,
                         OriginalScene = originalScene,
                     };
+                    break;
                 case nameof(SphereScene):
-                    ReadEndObject(ref reader);
-
-                    return new SphereScene();
-                default:
-                    throw new JsonException();
-
+                    scene = new SphereScene();
+                    break;
             }
-        }
-
-        private static void ReadStartObject(ref Utf8JsonReader reader)
-        {
-            if (reader.TokenType != JsonTokenType.StartObject && (!reader.Read() || reader.TokenType != JsonTokenType.StartObject))
-            {
-                throw new JsonException();
-            }
-        }
-
-        private static void ReadPropertyName(ref Utf8JsonReader reader, string propertyName)
-        {
-            if (!reader.Read() || reader.TokenType != JsonTokenType.PropertyName)
-            {
-                throw new JsonException();
-            }
-
-            if (!string.Equals(reader.GetString(), propertyName, StringComparison.InvariantCultureIgnoreCase))
-            {
-                throw new JsonException();
-            }
-        }
-
-        private static double ReadDoubleProperty(ref Utf8JsonReader reader, string propertyName)
-        {
-            ReadPropertyName(ref reader, propertyName);
-
-            if (!reader.Read() || reader.TokenType != JsonTokenType.Number)
-            {
-                throw new JsonException();
-            }
-
-            return reader.GetDouble();
-        }
-
-        private static string ReadStringProperty(ref Utf8JsonReader reader, string propertyName)
-        {
-            ReadPropertyName(ref reader, propertyName);
-
-            if (!reader.Read() || reader.TokenType != JsonTokenType.String)
-            {
-                throw new JsonException();
-            }
-
-            return reader.GetString();
-        }
-
-        private static void ReadEndObject(ref Utf8JsonReader reader)
-        {
-            if (!reader.Read() || reader.TokenType != JsonTokenType.EndObject)
-            {
-                throw new JsonException();
-            }
+            ReadEndObject(ref reader);
+            return scene;
         }
 
         public override void Write(Utf8JsonWriter writer, Scene value, JsonSerializerOptions options)
@@ -99,47 +43,17 @@ namespace Scenery.Controllers.Converters
 
         private static void WriteScene(Utf8JsonWriter writer, Scene scene)
         {
-            writer.WriteStartObject();
+            WriteStartObject(writer);
+            WriteStringInCamelCase(writer, nameof(Type), scene.GetType().Name);
             switch (scene)
             {
                 case ScaledScene scaled:
-                    WriteStringInCamelCase(writer, nameof(Type), nameof(ScaledScene));
                     WriteNumber(writer, nameof(ScaledScene.Factor), scaled.Factor);
                     WritePropertyName(writer, nameof(ScaledScene.OriginalScene));
                     WriteScene(writer, scaled.OriginalScene);
                     break;
-                case SphereScene sphere:
-                    WriteStringInCamelCase(writer, nameof(Type), nameof(SphereScene));
-                    break;
-                default:
-                    throw new JsonException();
             }
-            writer.WriteEndObject();
-        }
-
-        private static void WritePropertyName(Utf8JsonWriter writer, string propertyName)
-        {
-            writer.WritePropertyName(ConvertToCamelCase(propertyName));
-        }
-
-        private static void WriteNumber(Utf8JsonWriter writer, string propertyName, double value)
-        {
-            writer.WriteNumber(ConvertToCamelCase(propertyName), value);
-        }
-
-        private static void WriteStringInCamelCase(Utf8JsonWriter writer, string propertyName, string value)
-        {
-            writer.WriteString(ConvertToCamelCase(propertyName), ConvertToCamelCase(value));
-        }
-
-        private static string ConvertToCamelCase(string value)
-        {
-            return $"{char.ToLowerInvariant(value[0])}{value[1..]}";
-        }
-
-        private static string ConvertToPascalCase(string value)
-        {
-            return $"{char.ToUpperInvariant(value[0])}{value[1..]}";
+            WriteEndObject(writer);
         }
     }
 }
