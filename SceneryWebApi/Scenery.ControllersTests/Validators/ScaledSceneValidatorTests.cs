4231d1204387e4171e4e523e39343e9089cc10da
Daniel Snouck <dsnouck@gmail.com>
2021-11-02 21:22:00 +0100

    Create separate test classes for testing each validator.

--- SceneryWebApi/Scenery.ControllersTests/Validators/ScaledSceneValidatorTests.cs
+++ SceneryWebApi/Scenery.ControllersTests/Validators/ScaledSceneValidatorTests.cs
@@ -1,31 +1,30 @@
-﻿// <copyright file="SceneContainerValidatorTests.cs" company="dsnouck">
+﻿// <copyright file="ScaledSceneValidatorTests.cs" company="dsnouck">
 // Copyright (c) dsnouck. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Scenery.ControllersTests.Validators
 {
-    using System;
-    using FluentAssertions;
     using FluentValidation.TestHelper;
     using Moq;
     using Scenery.Components.Interfaces;
     using Scenery.Controllers.Validators;
     using Scenery.Models;
+    using Scenery.Models.Scenes;
     using Xunit;
 
     /// <summary>
     /// Provides tests for <see cref="SceneContainerValidator"/>.
     /// </summary>
-    public class SceneContainerValidatorTests
+    public class ScaledSceneValidatorTests
     {
         private readonly SceneContainerValidator systemUnderTest;
         private readonly Mock<IVector3Component> vector3ComponentTestDouble;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SceneContainerValidatorTests"/> class.
+        /// Initializes a new instance of the <see cref="ScaledSceneValidatorTests"/> class.
         /// </summary>
-        public SceneContainerValidatorTests()
+        public ScaledSceneValidatorTests()
         {
             this.vector3ComponentTestDouble = new Mock<IVector3Component>();
             this.vector3ComponentTestDouble
@@ -39,35 +38,22 @@ namespace Scenery.ControllersTests.Validators
         /// Tests <see cref="SceneContainerValidator"/>.
         /// </summary>
         [Fact]
-        public void GivenSceneContainerIsNullWhenValidateIsCalledThenAnArgumentNullExceptionIsThrown()
-        {
-            // Arrange.
-            SceneContainer sceneContainer = null;
-
-            // Act.
-            Action action = () => this.systemUnderTest.TestValidate(sceneContainer);
-
-            // Assert.
-            action.Should().Throw<ArgumentNullException>();
-        }
-
-        /// <summary>
-        /// Tests <see cref="SceneContainerValidator"/>.
-        /// </summary>
-        [Fact]
-        public void GivenSceneIsNullWhenValidateIsCalledThenItFails()
+        public void GivenOriginalSceneOfScaledSceneIsNullWhenValidateIsCalledThenItFails()
         {
             // Arrange.
             var sceneContainer = new SceneContainer
             {
-                Scene = null,
+                Scene = new ScaledScene
+                {
+                    OriginalScene = null,
+                },
             };
 
             // Act.
             var result = this.systemUnderTest.TestValidate(sceneContainer);
 
             // Assert.
-            result.ShouldHaveValidationErrorFor(sceneContainer => sceneContainer.Scene);
+            result.ShouldHaveValidationErrorFor(sceneContainer => (sceneContainer.Scene as ScaledScene).OriginalScene);
         }
     }
 }
--- SceneryWebApi/Scenery.ControllersTests/Validators/SceneValidatorTests.cs
+++ SceneryWebApi/Scenery.ControllersTests/Validators/SceneValidatorTests.cs
@@ -1,31 +1,30 @@
-﻿// <copyright file="SceneContainerValidatorTests.cs" company="dsnouck">
+﻿// <copyright file="SceneValidatorTests.cs" company="dsnouck">
 // Copyright (c) dsnouck. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Scenery.ControllersTests.Validators
 {
-    using System;
-    using FluentAssertions;
     using FluentValidation.TestHelper;
     using Moq;
     using Scenery.Components.Interfaces;
     using Scenery.Controllers.Validators;
     using Scenery.Models;
+    using Scenery.Models.Scenes;
     using Xunit;
 
     /// <summary>
-    /// Provides tests for <see cref="SceneContainerValidator"/>.
+    /// Provides tests for <see cref="SceneValidator"/>.
     /// </summary>
-    public class SceneContainerValidatorTests
+    public class SceneValidatorTests
     {
         private readonly SceneContainerValidator systemUnderTest;
         private readonly Mock<IVector3Component> vector3ComponentTestDouble;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SceneContainerValidatorTests"/> class.
+        /// Initializes a new instance of the <see cref="SceneValidatorTests"/> class.
         /// </summary>
-        public SceneContainerValidatorTests()
+        public SceneValidatorTests()
         {
             this.vector3ComponentTestDouble = new Mock<IVector3Component>();
             this.vector3ComponentTestDouble
@@ -39,28 +38,12 @@ namespace Scenery.ControllersTests.Validators
         /// Tests <see cref="SceneContainerValidator"/>.
         /// </summary>
         [Fact]
-        public void GivenSceneContainerIsNullWhenValidateIsCalledThenAnArgumentNullExceptionIsThrown()
-        {
-            // Arrange.
-            SceneContainer sceneContainer = null;
-
-            // Act.
-            Action action = () => this.systemUnderTest.TestValidate(sceneContainer);
-
-            // Assert.
-            action.Should().Throw<ArgumentNullException>();
-        }
-
-        /// <summary>
-        /// Tests <see cref="SceneContainerValidator"/>.
-        /// </summary>
-        [Fact]
-        public void GivenSceneIsNullWhenValidateIsCalledThenItFails()
+        public void GivenSceneIsOfTypeSceneWhenValidateIsCalledThenItFails()
         {
             // Arrange.
             var sceneContainer = new SceneContainer
             {
-                Scene = null,
+                Scene = new Scene(),
             };
 
             // Act.
