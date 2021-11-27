02ef25e0cde095d79cc35a0e7be66bca6ba419ab
Daniel Snouck <dsnouck@gmail.com>
2021-11-27 21:00:18 +0100

    Add Senery.IntegrationTests.

---
+++ SceneryWebApi/Scenery.IntegrationTests/RenderingTests.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="RenderingTests.cs" company="dsnouck">
+// Copyright (c) dsnouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Scenery.IntegrationTests
+{
+    using Xunit;
+
+    /// <summary>
+    /// Provides integration tests for rendering scenes.
+    /// </summary>
+    public class RenderingTests
+    {
+        /// <summary>
+        /// Tests the WebApi.
+        /// </summary>
+        [Fact]
+        public void GivenASceneContainerWhenPostIsCalledThenItIsCorrectlyRendered()
+        {
+            // TODO: Implement.
+            // Arrange.
+
+            // Act.
+
+            // Assert.
+        }
+    }
+}
